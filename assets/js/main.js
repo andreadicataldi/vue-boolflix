f4e6b9d87ea6be9451d77700c2d970cad252ff5c
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2021-01-13 13:21:10 +0100

    added tv series

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@ let app = new Vue({
   el: "#app",
   data: {
     movies: [],
+    series: [],
     searchText: "",
     counter: 0,
   },
@@ -15,6 +16,21 @@ let app = new Vue({
         .then((response) => {
           this.movies = response.data.results;
           this.movies.forEach((element) => {
+            let score = Math.ceil(element.vote_average / 2);
+            element.vote_average = score;
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+      axios
+        .get(
+          "https://api.themoviedb.org/3/search/tv?api_key=1a404737bd28f16647b6ba305c98e1ed&language=it_IT&query=" +
+            searchText
+        )
+        .then((response) => {
+          this.series = response.data.results;
+          this.series.forEach((element) => {
             let score = Math.ceil(element.vote_average / 2);
             element.vote_average = score;
           });
